c646ed8d1e7a5b50d9c253cce4c4ff5b5813d41b
Jacob Pyatt <herobbrine102@gmail.com>
2023-03-30 20:35:48 -0400

    Finds most chunks and formats mostly correct

--- rez_to_csv.py
+++ rez_to_csv.py
@@ -1,6 +1,16 @@
 import os
 import PySimpleGUI as sg
 
+#function to convert a string to all lowercase
+def tolower(string):
+    new_string = ""
+    for char in string:
+        if char.isupper():
+            new_string = new_string + char.lower()
+        else:
+            new_string = new_string + char
+    return new_string
+
 #define main function
 def main():
     #using PySimpleGUI find the folder with files to be converted
@@ -14,8 +24,9 @@ def main():
     path = values[0]
     #get the list of files in the folder
     list_of_folders = os.listdir(path)
-    #create a new folder to output to
-    os.mkdir(path + "/../outputs")
+    #create a new folder to output to if it doesn't already exist
+    if not os.path.exists(path + "/../outputs"):
+        os.mkdir(path + "/../outputs")
 
     #loop through the list of folders
     for folder in list_of_folders:
@@ -41,10 +52,11 @@ def main():
         #loop through the list of lines in the chunk.csv file
         chunk_lines = []
         for line in chunk_file:
-            #get rid of everything between quotation marks
+            #get rid of everything before and includeing the first closing bracket
             first = line.find('"')
             second = line.find('"', first + 1)
             line = line[second + 2:]
+            line = line.replace('"', "")
             #split the line by commas
             line = line.split(",")
             #add the split line to new variable
@@ -52,20 +64,16 @@ def main():
         #loop through the list of lines in the csv file
         new_csv_file = []
         for line in csv_file:
+            line_compare = tolower(line)
             for chunk in chunk_lines:
-                if line.find(chunk[0]) != -1:
+                if line_compare.find(chunk[0]) != -1:
                     line = line + "," + chunk[0]
-                    chunk = chunk[1:]
-                    #join the chunk back together
-                    chunk = "".join(chunk)
-                    #get rid of all commas
-                    chunk = chunk.replace(",", "")
-                    #add the chunk to the line
-                    line = line + chunk
+                    for i in range(1, len(chunk)):
+                        if chunk[i] != ", " and chunk[i] != "" and chunk[i] != " ":
+                            line = line + "," + chunk[i]
+
 
             new_csv_file.append(line)
-
-
         #write the new csv file to the outputs folder
         with open(path + "/../outputs/" + file_name, "w", encoding="utf-8") as f:
             for line in new_csv_file:
